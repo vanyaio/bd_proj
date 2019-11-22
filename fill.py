50c16ccb2162bf417e67513700903d53b253365f
Vanya Io <differentstuff777@gmail.com>
2019-11-22 22:02:57 +0300

    yaml added

--- fill.py
+++ fill.py
@@ -4,6 +4,7 @@ from datetime import date
 from datetime import datetime
 import random
 from exam import *
+import yaml
 
 def get_rand_day():
     start_dt = date.today().replace(day=1, month=1).toordinal()
@@ -108,61 +109,83 @@ def get_exams(subjs):
         exams.append(exam2)
     return exams
 
-def fill_data():
-    tables_create()
-    districts = get_districts()
-    schools = get_schools(districts)
-    classrooms = get_classrooms(schools)
-    students = get_students(schools)
-    subjs = get_subjs()
-    req_subjs = get_req_subjs(subjs)
-    student_subjs = get_student_subjs(students, subjs, req_subjs)
-    exams = get_exams(subjs)
+def fill_data(from_conf):
+    if from_conf:
+        with open('conf/districts') as f:
+            districts = yaml.load(f, Loader=yaml.FullLoader)
+        with open('conf/schools') as f:
+            schools = yaml.load(f, Loader=yaml.FullLoader)
+        with open('conf/classrooms') as f:
+            classrooms = yaml.load(f, Loader=yaml.FullLoader)
+        with open('conf/students') as f:
+            students = yaml.load(f, Loader=yaml.FullLoader)
+        with open('conf/subjs') as f:
+            subjs = yaml.load(f, Loader=yaml.FullLoader)
+        with open('conf/req_subjs') as f:
+            req_subjs = yaml.load(f, Loader=yaml.FullLoader)
+        with open('conf/exams') as f:
+            exams = yaml.load(f, Loader=yaml.FullLoader)
+        with open('conf/student_subjs') as f:
+            student_subjs = yaml.load(f, Loader=yaml.FullLoader)
+        with open('conf/grades') as f:
+            grades = yaml.load(f, Loader=yaml.FullLoader)
+        with open('conf/exam_distribs') as f:
+            exam_distribs = yaml.load(f, Loader=yaml.FullLoader)
+        with open('conf/student_distribs') as f:
+            student_distribs = yaml.load(f, Loader=yaml.FullLoader)
+    else:
+        districts = get_districts()
+        schools = get_schools(districts)
+        classrooms = get_classrooms(schools)
+        students = get_students(schools)
+        subjs = get_subjs()
+        req_subjs = get_req_subjs(subjs)
+        student_subjs = get_student_subjs(students, subjs, req_subjs)
+        exams = get_exams(subjs)
 
-    #  grades, rers, student_rers, exam_distribs, student_distrib
-    exam_distribs = []
-    for classroom in classrooms:
-        for exam in exams:
-            exam_distrib = {}
-            exam_distrib['id'] = len(exam_distribs) + 1
-            exam_distrib['classroom_id'] = classroom['id']
-            exam_distrib['exam_id'] = exam['id']
-            exam_distribs.append(exam_distrib)
-
-    student_distribs = []
-    grades = []
-    for student in students:
-        this_student_subjs = []
-        for i in student_subjs:
-            if (i['student_id'] == student['id']):
-                this_student_subjs.append(i['subj_id'])
-
-        for subj_id in this_student_subjs:
-            subj_exam = {}
-            dec31 = datetime.strptime('Dec 31 2019', '%b %d %Y').date()
-            subj_exam['day'] = dec31
+        #  grades, rers, student_rers, exam_distribs, student_distrib
+        exam_distribs = []
+        for classroom in classrooms:
             for exam in exams:
-                #  if (exam['subj_id'] == subj['id'] and \
-                    #  exam['day'] <= subj_exam['day']):
-                if (exam['subj_id'] == subj_id and \
-                    exam['day'] <= subj_exam['day']):
-                    subj_exam = exam
+                exam_distrib = {}
+                exam_distrib['id'] = len(exam_distribs) + 1
+                exam_distrib['classroom_id'] = classroom['id']
+                exam_distrib['exam_id'] = exam['id']
+                exam_distribs.append(exam_distrib)
 
-            exam_distrib = {}
-            student_distrib = {}
-            for i in exam_distribs:
-                if (i['exam_id'] == subj_exam['id']):
-                    student_distrib['student_id'] = student['id']
-                    student_distrib['exam_distrib_id'] = i['id'] 
+        student_distribs = []
+        grades = []
+        for student in students:
+            this_student_subjs = []
+            for i in student_subjs:
+                if (i['student_id'] == student['id']):
+                    this_student_subjs.append(i['subj_id'])
 
-            student_distribs.append(student_distrib)
+            for subj_id in this_student_subjs:
+                subj_exam = {}
+                dec31 = datetime.strptime('Dec 31 2019', '%b %d %Y').date()
+                subj_exam['day'] = dec31
+                for exam in exams:
+                    #  if (exam['subj_id'] == subj['id'] and \
+                        #  exam['day'] <= subj_exam['day']):
+                    if (exam['subj_id'] == subj_id and \
+                        exam['day'] <= subj_exam['day']):
+                        subj_exam = exam
 
-            grade = {}
-            grade['student_id'] = student['id']
-            grade['exam_id'] = subj_exam['id']
-            grade['grade'] = random.randint(0, 100)
-            grades.append(grade)
+                exam_distrib = {}
+                student_distrib = {}
+                for i in exam_distribs:
+                    if (i['exam_id'] == subj_exam['id']):
+                        student_distrib['student_id'] = student['id']
+                        student_distrib['exam_distrib_id'] = i['id'] 
 
+                student_distribs.append(student_distrib)
+
+                grade = {}
+                grade['student_id'] = student['id']
+                grade['exam_id'] = subj_exam['id']
+                grade['grade'] = random.randint(0, 100)
+                grades.append(grade)
 
 
 
@@ -212,5 +235,29 @@ def fill_data():
         student_distrib_add_raw(student_distrib['student_id'],
                                 student_distrib['exam_distrib_id'])
 
+    with open('conf/districts', 'w') as f:
+        yaml.dump(districts, f)
+    with open('conf/schools', 'w') as f:
+        yaml.dump(schools, f)
+    with open('conf/classrooms', 'w') as f:
+        yaml.dump(classrooms, f)
+    with open('conf/students', 'w') as f:
+        yaml.dump(students, f)
+    with open('conf/subjs', 'w') as f:
+        yaml.dump(subjs, f)
+    with open('conf/req_subjs', 'w') as f:
+        yaml.dump(req_subjs, f)
+    with open('conf/exams', 'w') as f:
+        yaml.dump(exams, f)
+    with open('conf/student_subjs', 'w') as f:
+        yaml.dump(student_subjs, f)
+    with open('conf/grades', 'w') as f:
+        yaml.dump(grades, f)
+    with open('conf/exam_distribs', 'w') as f:
+        yaml.dump(exam_distribs, f)
+    with open('conf/student_distribs', 'w') as f:
+        yaml.dump(student_distribs, f)
+
 if __name__ == "__main__":
-    fill_data()
+    #  tables_create()
+    fill_data(True)
