7f9d2bc0079f9d3d8f3431893af9a0144ddd0d26
Vanya Io <differentstuff777@gmail.com>
2019-11-27 02:25:20 +0300

    rm useful files

--- a.sql
+++
@@ -1,9 +0,0 @@
-create table weather (
-    city            varchar(80),
-    temp_lo         int,           -- low temperature
-    temp_hi         int,           -- high temperature
-    prcp            real,          -- precipitation
-    date            date
-);
-select * from weather;
-drop table weather;
